d992b9359e7bcf40632ad63d27e6e7e578348bd1
Artistic Software <artisticsoftware7@gmail.com>
2023-07-30 08:11:16 +0000

    ...

---
+++ archive/0628/probas.php
@@ -0,0 +1,68 @@
+<?php
+
+return [
+	'2' => [
+		/**
+		Race 2
+		*/
+		'qpl' => '3 X 1',
+		//If qpl payout > 50 HKD.
+		'Bet' =>  '9, ',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'qpl' => '12 X 1, 3, 5, 8, 10',
+		//If qpl payout > 50 HKD.
+		'Bet' =>  '9, ',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'qpl' => '9 X 1, 6, 8, 12',
+		//If qpl payout > 100 HKD.
+		'Bet' =>  '9, ',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'qpl' => '1 X 3, 5, 6, 10, 11',
+		//If qpl payout > 100 HKD.
+		'Bet' =>  '',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'qpl' => '6 X 4, 12',
+		//If qpl payout > 50 HKD.
+		'Bet' =>  '7, ',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'qpl' => '1 X 2, 4, 11',
+		//If qpl payout > 100 HKD.
+		'Bet' =>  '6, ',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'qpl' => '4 X ',
+		//If qpl payout > 50 HKD.
+		'Bet' =>  '5, 9, 7, ',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'qpl' => '2 X 3, 6, 9',
+		//If qpl payout > 100 HKD.
+		'Bet' =>  '',
+	],
+];
